48071fd3a5784c306c22419cd8dc209c9fd19a57
Pavel Yakovlev <thrnrhhl@yandex.ru>
2025-03-27 21:26:23 +0300

    UNT-18 [shared-ui] Add css assembly

---
+++ src/main.tsx
@@ -0,0 +1,33 @@
+import "./index.css";
+
+export { Button } from "./ui/button";
+export { Description } from "./ui/description";
+export { Drawer } from "./ui/drawer";
+export { FormError } from "./ui/form-error";
+export { Input } from "./ui/input";
+export { Label } from "./ui/label";
+export {
+  Select,
+  SelectLabel,
+  SelectOption,
+  getSelectMultipleValue,
+  getSelectSingleValue,
+} from "./ui/select";
+export { Textarea } from "./ui/textarea";
+
+export { cn, useIsMobile } from "./lib";
+export { Text } from "./ui/text";
+
+export {
+  type GenericTableColumnType,
+  GenericTable,
+  GenericTableSearch,
+  useGenericTableFilter,
+  GenericTableRowAction,
+} from "./ui/generic-table";
+
+export { Container } from "./ui/container";
+export { Wrapper } from "./ui/wrapper";
+export { Field } from "./ui/field";
+
+export { NavLink, NavTitle } from "./ui/nav";
